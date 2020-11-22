0eabf5c673342ef9f78142c53eae307e58b5ba2e
Cian Tudur <74679797+ciantudur@users.noreply.github.com>
2020-11-22 23:39:50 +0000

    Update prepare_data.R
    Used 1 loop instead of 24 for seasonality adjustment & switch to saving file in .feather format.

--- prepare_data.R
+++ prepare_data.R
@@ -23,7 +23,7 @@ setwd("~/R/Google") # CHANGE AS REQUIRED
 ## INSTALL REQUIRED PACKAGES ---------------------------------------------------
 list.of.packages <- c(
   "sf", "ggplot2", "here", "magrittr", "tidyverse", "readr",
-  "RCurl", "datasets", "ggpubr", "imputeTS", "plyr", "forecast"
+  "RCurl", "datasets", "ggpubr", "imputeTS", "plyr", "forecast", "feather"
 )
 new.packages <- list.of.packages[!(list.of.packages %in%
   installed.packages()[, "Package"])]
@@ -100,6 +100,7 @@ imputed_data <- imputed_data[order(
   imputed_data$sub_region_1
 ), ]
 
+
 # Use population weights to calculate weighted average
 pop_weights <- read.csv("pop_weights_19.csv", sep = ";")
 pop_weights$pop_19_2 <- as.numeric(pop_weights$pop_19)
@@ -118,6 +119,8 @@ imputed_data_2$sub_region_1 <- "Wales"
 imputed_data_2$iso_3166_2_code <- "Wales"
 imputed_data_2$la_name_alt <- NULL
 
+
+# Loop this??
 imputed_data_2$retail_and_recreation_percent_change_from_baseline <-
   sapply(
     split(imputed_data, imputed_data$date),
@@ -199,7 +202,7 @@ all_wales$country <- "Wales"
 names(all_wales)[1] <- "date"
 
 google_data_uk_series$country <- "UK"
-google_data_uk_series <- google_data_uk_series[, 8:15]
+google_data_uk_series <- google_data_uk_series[,8:15]
 
 uk_wales_data <- rbind(all_wales, google_data_uk_series)
 uk_wales_data[, 2:7] <- uk_wales_data[, 2:7] / 100
@@ -210,12 +213,9 @@ la_data <- imputed_data[, 8:15]
 la_data$country <- la_data$la_name_alt
 la_data$la_name_alt <- NULL
 
-la_data[, 2] <- la_data[, 2] / 100
-la_data[, 3] <- la_data[, 3] / 100
-la_data[, 4] <- la_data[, 4] / 100
-la_data[, 5] <- la_data[, 5] / 100
-la_data[, 6] <- la_data[, 6] / 100
-la_data[, 7] <- la_data[, 7] / 100
+for (i in 2:7) {
+  la_data[, i] <- la_data[, i] / 100
+}
 
 uk_wales_data <- rbind(la_data, uk_wales_data)
 
@@ -225,283 +225,29 @@ uk_wales_data <- rbind(la_data, uk_wales_data)
 ## SEASONALITY ADJUSTMENT ------------------------------------------------------
 uk_wales_data_sa <- uk_wales_data
 
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Wales", i]
-  ts_temp <- ts(data_temp, frequency = 7)
-  decompose_temp <- decompose(ts_temp, "additive")
-  uk_wales_data_sa[uk_wales_data_sa$country == "Wales", i] <-
-    decompose_temp$trend
-}
 
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "UK", i]
-  ts_temp <- ts(data_temp, frequency = 7)
-  decompose_temp <- decompose(ts_temp, "additive")
-  uk_wales_data_sa[uk_wales_data_sa$country == "UK", i] <- decompose_temp$trend
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Blaenau Gwent", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Blaenau Gwent", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Blaenau Gwent", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Bridgend", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Bridgend", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Bridgend", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Carmarthenshire", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Carmarthenshire", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Carmarthenshire", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Caerphilly", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Caerphilly", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Caerphilly", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Ceredigion", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Ceredigion", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Ceredigion", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Conwy", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Conwy", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Conwy", i] <- NA
-  }
-}
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Denbighshire", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Denbighshire", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Denbighshire", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Flintshire", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Flintshire", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Flintshire", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Gwynedd", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Gwynedd", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Gwynedd", i] <- NA
-  }
-}
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Isle of Anglesey", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Isle of Anglesey", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Isle of Anglesey", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Merthyr Tydfil", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Merthyr Tydfil", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Merthyr Tydfil", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Monmouthshire", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Monmouthshire", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Monmouthshire", i] <- NA
-  }
-}
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Neath Port Talbot", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Neath Port Talbot", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Neath Port Talbot", i] <- NA
-  }
-}
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Newport", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Newport", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Newport", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Pembrokeshire", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Pembrokeshire", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Pembrokeshire", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Powys", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Powys", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Powys", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Rhondda Cynon Taf", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Rhondda Cynon Taf", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Rhondda Cynon Taf", i] <- NA
-  }
-}
+# List of areas
+areas <- c("Wales", "UK", "Blaenau Gwent", "Bridgend", "Cardiff", "Carmarthenshire",
+               "Caerphilly", "Ceredigion", "Conwy", "Denbighshire", "Flintshire",
+               "Gwynedd", "Isle of Anglesey", "Merthyr Tydfil", "Monmouthshire",
+               "Neath Port Talbot", "Newport", "Pembrokeshire", "Powys",
+               "Rhondda Cynon Taf", "Swansea", "Torfaen", "Vale of Glamorgan",
+               "Wrexham")
 
 
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Swansea", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Swansea", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Swansea", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Torfaen", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Torfaen", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Torfaen", i] <- NA
+# Seasonally adjust data using ARIMA model
+for (i in areas) {
+  for (j in 2:7) {
+    data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == i, j]
+    if (all(!is.na(data_temp))) {
+      ts_temp <- ts(data_temp, frequency = 7)
+      decompose_temp <- decompose(ts_temp, "additive")
+      uk_wales_data_sa[uk_wales_data_sa$country == i, j] <- decompose_temp$trend
+    } else {
+      uk_wales_data_sa[uk_wales_data_sa$country == i, j] <- NA
+    }
   }
 }
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Cardiff", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Cardiff", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Cardiff", i] <- NA
-  }
-}
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Vale of Glamorgan", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Vale of Glamorgan", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Vale of Glamorgan", i] <- NA
-  }
-}
-
-
-for (i in 2:7) {
-  data_temp <- uk_wales_data_sa[uk_wales_data_sa$country == "Wrexham", i]
-  if (all(!is.na(data_temp))) {
-    ts_temp <- ts(data_temp, frequency = 7)
-    decompose_temp <- decompose(ts_temp, "additive")
-    uk_wales_data_sa[uk_wales_data_sa$country == "Wrexham", i] <- decompose_temp$trend
-  }
-  else {
-    uk_wales_data_sa[uk_wales_data_sa$country == "Wrexham", i] <- NA
-  }
-}
-
 
 
 uk_wales_data_sa <- uk_wales_data_sa[
@@ -511,6 +257,6 @@ uk_wales_data_sa <- uk_wales_data_sa[
 
 
 ## SAVE DATA FOR PLOTTING ------------------------------------------------------
-now <- Sys.time()
 file_name <- "google_data.csv"
-write.csv(uk_wales_data_sa, paste0(file_name), row.names = TRUE)
+write_feather(uk_wales_data_sa, "google_data.feather")
+
